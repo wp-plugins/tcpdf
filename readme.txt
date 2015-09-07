14174273bfa453ed5fa203a127b50de9ba146907
rheinardkorf <rheinardkorf@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-07 14:37:22 +0000

    * Note: Project re-activated. * Update: Now released with TCPDF 6.2.11. * Update: New wrapper class for loading TCPDF. * Update: Sets as first plugin upon activation to ensure that TCPDF is available when needed.
    git-svn-id: https://plugins.svn.wordpress.org/tcpdf/trunk@1239877 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -1,17 +1,20 @@
 === TCPDF Library ===
-Contributors: sushkov
-Tags: tcpdf library pdf
-Requires at least: WordPress 1.0
-Tested up to: WordPress 3.4.2
+Contributors: rheinardkorf, sushkov
+Tags: tcpdf, pdf, developer
+Requires at least: WordPress 4.0
+Tested up to: WordPress 4.3
 Stable tag: trunk
-Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=stas@net.utcluj.ro
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Donate link: https://www.paypal.me/rheinard
 
-Pluginified TCPDF library to be used as a dependency.
+A WordPress wrapper for the popular TCPDF Library.
 
 == Description ==
 
-Pluginified TCPDF library to be used as a dependency
-instead of bundling all the files with a new plugin.
+Exposes the TCPDF library to be used by other plugins or themes. This greatly
+reduces the size of plugins and will ensure that you always have the latest
+TCPDF version (compatible with WordPress minimum requirements) available.
 
 == Installation ==
 
@@ -19,9 +22,18 @@ Please follow the [standard installation procedure for WordPress plugins](http:/
 
 == Frequently Asked Questions ==
 
-For questions, please email me mailto:stas@net.utcluj.ro
+** How do I use this plugin? **
+
+This plugin on its own is not very useful. It gets used by other plugins that
+require the ability to produce PDF documents.
 
 == Changelog ==
+
+= 1.0 =
+* Note: Project re-activated.
+* Update: Now released with TCPDF 6.2.11.
+* Update: New wrapper class for loading TCPDF.
+* Update: Sets as first plugin upon activation to ensure that TCPDF is available when needed.
 
 = 0.1 =
 * First stable release.
--- tcpdf.php
+++ tcpdf.php
@@ -1,34 +1,87 @@
 <?php
 /*
 Plugin Name: TCPDF Library
-Plugin URI: http://wordpress.org/extend/plugins/tcpdf/
-Description: TCPDF Library as a plugin
-Version: 0.1
-Author: sushkov
+Version: 1.0
+Description: Provides the TCPDF library for creating PDF documents. This plugin is not useful on its own and is used by other plugins to create PDF documents.
+Author: Rheinard Korf
 Author URI: http://wordpress.org/extend/plugins/tcpdf/
-*/
-?>
-<?php
-/*  Copyright 2012 Stas Suscov <stas@net.utcluj.ro>
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+Plugin URI: http://wordpress.org/extend/plugins/tcpdf/
+Developers: Stas Sușcov ( https://twitter.com/Suscov ), Rheinard Korf ( https://twitter.com/rheinardkorf )
+License: GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
 */
 
-define( 'TCPDF_VERSION_LOADED', '5.9.3' );
+/**
+ * Contributors: Stas Sușcov, Rheinard Korf (Incsub)
+ *
+ * @license https://www.gnu.org/licenses/gpl-2.0.html GNU General Public License, version 2 (GPL-2.0)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+ * MA 02110-1301 USA
+ *
+ */
 
-if ( !class_exists( TCPDF ) ) {
-  require( dirname( __FILE__ ) . '/lib/tcpdf/tcpdf.php' );
+
+/**
+ * Class TCPDF_Loader
+ *
+ * This class is a WordPress wrapper for the TCPDF library. It will attempt to ensure that this plugin is the
+ * first call to TCPDF by hooking `activated_plugin` and adding it as the first item in the array.
+ *
+ * Once activated, TCPDF will be available to all other plugins.
+ *
+ */
+class TCPDF_Loader {
+
+	/**
+	 * Initialise the TCPDF Library plugin
+	 */
+	public static function init() {
+
+		add_action( 'activated_plugin', array( __CLASS__, 'load_tcpdf_first' ) );
+
+		if ( ! class_exists( 'TCPDF' ) ) {
+			define( 'TCPDF_PLUGIN_ACTIVE', true );
+			define( 'TCPDF_VERSION', '6.2.11' );
+			require( dirname( __FILE__ ) . '/lib/tcpdf/tcpdf.php' );
+		}
+
+	}
+
+	/**
+	 * When plugins are activated, make sure that TCPDF is first.
+	 *
+	 * Note: This is just a standard hook. Other plugins can still jump to first if hooking `activated_plugin`
+	 */
+	public static function load_tcpdf_first() {
+		$path            = __FILE__;
+		$path            = str_replace( trailingslashit( WP_PLUGIN_DIR ), '', $path );
+		$path            = str_replace( WP_CONTENT_DIR . '/mu-plugins/', '', $path );
+		$active_plugins  = get_option( 'active_plugins' );
+		$this_plugin_key = array_search( $path, $active_plugins );
+		if ( $this_plugin_key ) { // if it's 0 it's the first plugin already, no need to continue
+			array_splice( $active_plugins, $this_plugin_key, 1 );
+			array_unshift( $active_plugins, $path );
+			update_option( 'active_plugins', $active_plugins );
+		}
+	}
+
 }
-?>
+
+// Keep it tidy in a class
+TCPDF_Loader::init();
+
+
+
+
